0e5d2d8eed11fe43fd78d2918b13dab66ef8a9ee
Yash <yashn49@gmail.com>
2020-10-31 08:40:02 +0530

    last name

--- UserRegistrationProblem.sh
+++ UserRegistrationProblem.sh
@@ -9,3 +9,13 @@ if [[ $firstName =~ $regex ]]
  else
         echo "false"
 fi
+
+shopt -s extglob
+regex1="^([A-Z]{1}[a-zA-Z]{2,})+$"
+read -p "enter a valid last name " lastName
+if [[ $lastName =~ $regex1 ]]
+ then
+        echo "true"
+ else
+        echo "false"
+fi
