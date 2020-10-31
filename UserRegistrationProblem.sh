bf597e1dd8a7ff40f4a9c50a96f1237031f62a3a
Yash <yashn49@gmail.com>
2020-10-31 08:26:36 +0530

    first name

--- UserRegistrationProblem.sh
+++ UserRegistrationProblem.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-
 echo "Welcome to user registration program"
+shopt -s extglob
+regex="^([A-Z]{1}[a-zA-Z]{2,})+$"
+read -p "enter a valid first name " firstName
+if [[ $firstName =~ $regex ]]
+ then
+        echo "true"
+ else
+        echo "false"
+fi
